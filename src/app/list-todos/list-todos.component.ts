e996fdf9986ed584da81e5cd485239f8fd1896c1
jesus doza <jesus.doza@gmail.com>
2023-06-08 09:03:39 -0500

    pipe data conversion

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 
+export class Todo {
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date
+  ) {}
+}
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -7,17 +16,8 @@ import { Component } from '@angular/core';
 })
 export class ListTodosComponent {
   todos = [
-    {
-      id: 1,
-      description: 'todo something',
-    },
-    {
-      id: 2,
-      description: 'something else',
-    },
-    {
-      id: 3,
-      description: 'fdasfasdf',
-    },
+    new Todo(12, 'this is desc', false, new Date()),
+    new Todo(14, 'this is desc14', false, new Date()),
+    new Todo(16, 'this fdsafasc', false, new Date()),
   ];
 }
