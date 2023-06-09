26ca0feb70ddccf67be1361367fc948f2c95b79f
jesus doza <jesus.doza@gmail.com>
2023-06-09 09:58:07 -0500

    add auth service

--- src/app/service/hardcoded-athentication.service.ts
+++ src/app/service/hardcoded-athentication.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HardcodedAthenticationService {
+  constructor() {}
+  authenticate(username: string, password: string) {
+    // console.log('user logged in before ? ', this.isUserLoggedIn());
+    if (username === 'bob' && password === 'bob') {
+      sessionStorage.setItem('authenticatedUser', username);
+      return true;
+    }
+    return false;
+  }
 
-  constructor() { }
+  isUserLoggedIn() {
+    const user = sessionStorage.getItem('authenticatedUser');
+    return !(user === null);
+  }
+
+  logout() {
+    sessionStorage.removeItem('authenticatedUser');
+  }
 }
