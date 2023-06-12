bcb42487c27cf7191b442c99f2c55d94343bbfbc
jesus doza <jesus.doza@gmail.com>
2023-06-12 18:25:21 -0500

    path variables

--- backend/rest-web-service/src/main/java/com/rest/webservice/restwebservice/controllers/HelloWorldController.java
+++ backend/rest-web-service/src/main/java/com/rest/webservice/restwebservice/controllers/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.rest.webservice.restwebservice.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +17,11 @@ public class HelloWorldController {
   @GetMapping(path = "/hello-world-bean")
   public HelloWorldBean helloWorldBean(){
     return new HelloWorldBean("hello world");
+  }
+
+  @GetMapping(path = "/hello-world/path-variable/{name}")
+  public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
+    var bob = "bob";
+    return new HelloWorldBean(String.format("Hello World, %1$s %2$s", name,bob));
   }
 }
