a4fc258936f6dc9e635a60399432b6bc019fd8ee
jesus doza <jesus.doza@gmail.com>
2023-06-13 08:56:53 -0500

    Add welcome data service

---
+++ src/app/service/data/welcome-data.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WelcomeDataService {
+  constructor() {}
+
+  executeBeanService() {
+    console.log('bean service called');
+  }
+}
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { WelcomeDataService } from '../service/data/welcome-data.service';
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
@@ -7,9 +8,18 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class WelcomeComponent {
   name = '';
+  welcomeMessage = `Welcome`;
   ngOnInit() {
     // get the route parameter with name key
     this.name = this.route.snapshot.params['name'];
   }
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private service: WelcomeDataService
+  ) {}
+
+  getWelcomeMessage() {
+    console.log('button clicked');
+    this.service.executeBeanService();
+  }
 }
